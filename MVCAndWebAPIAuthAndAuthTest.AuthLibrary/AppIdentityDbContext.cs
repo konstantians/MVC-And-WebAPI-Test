121ce80aa4dd83f761c9b5d5d02b61bed166a649
Konstantinos Kinnas <kinnaskonstantinos0@gmail.com>
2024-04-17 00:24:35 +0300

    Added Integration Tests For Email Rest API - Closes Issue 33
    In this update a new test project has been added and in that 7 integration tests have been also added, which test the 4 endpoints of the Email Rest API. These test will make sure concistency in future changes that might be added to the library. Some minor configuration updates have also been added to the project as a whole and some solution filters for easier management of the different microservices that are contained in the project.

--- MVCAndWebAPIAuthAndAuthTest.AuthLibrary/AppIdentityDbContext.cs
+++ MVCAndWebAPIAuthAndAuthTest.AuthLibrary/AppIdentityDbContext.cs
@@ -6,7 +6,7 @@ namespace SocialMediaApp.Authentication;
 
 public class AppIdentityDbContext : IdentityDbContext
 {
-    private readonly IConfiguration _configuration;
+    private readonly IConfiguration? _configuration;
 
     //used for migrations
     public AppIdentityDbContext()
@@ -31,8 +31,8 @@ public class AppIdentityDbContext : IdentityDbContext
         //otherwise this is used for migrations, because the configuration can not be instantiated without the application running
         else
         {
-            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Database=MVCAndWebAPIAuthDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False",
-                options => options.EnableRetryOnFailure());
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;AttachDbFilename=D:\\Visual Studio\\LocalDbDatabases\\MVCAndWebAPIAuthDb.mdf;Initial Catalog=MVCAndWebAPIAuthDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False",
+                options => options.EnableRetryOnFailure()); 
         }
     }
 }
---
+++ MVCAndWebAPIAuthAndAuthTest.EmailLibraryRestAPI.Tests/HelperMethods/ResetDatabaseHelperMethods.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Azure.Cosmos;
+using Microsoft.Data.SqlClient;
+
+namespace MVCAndWebAPIAuthAndAuthTest.EmailLibraryRestAPI.Tests.HelperMethods
+{
+    internal class ResetDatabaseHelperMethods
+    {
+        public static async Task ResetNoSqlEmailDatabase()
+        {
+            string cosmosDbConnectionString = Environment.GetEnvironmentVariable("CosmosDbConnectionString")!;
+
+            CosmosClient cosmosClient = new CosmosClient(cosmosDbConnectionString);
+            Database database = cosmosClient.GetDatabase("GlobalDb");
+            Container container = database.GetContainer("MVCAPITest_Emails");
+
+            //all the documents of the container
+            FeedIterator<dynamic> resultSetIterator = container.GetItemQueryIterator<dynamic>("SELECT * FROM c");
+
+            while (resultSetIterator.HasMoreResults)
+            {
+                //a batch of the documents that are loaded from resultSetIterator
+                FeedResponse<dynamic> response = await resultSetIterator.ReadNextAsync();
+
+                foreach (var document in response)
+                {
+                    await container.DeleteItemAsync<dynamic>(id: document.id.ToString(),
+                        partitionKey: new PartitionKey(document.Id.ToString()));
+                }
+            }
+
+            Console.WriteLine("All documents deleted from NoSql email database.");
+        }
+
+        public static void ResetSqlEmailDatabase()
+        {
+            string connectionString = Environment.GetEnvironmentVariable("SqlEmail")!;
+            string[] tables = new string[] { "dbo.Emails" };
+
+            using SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            foreach (string table in tables)
+            {
+                string deleteQuery = $"DELETE FROM {table}";
+                using (SqlCommand deleteCommand = new SqlCommand(deleteQuery, connection))
+                    deleteCommand.ExecuteNonQuery();
+            }
+
+            connection.Close();
+
+            Console.WriteLine("All documents deleted from Sql email database.");
+        }
+
+
+    }
+}
